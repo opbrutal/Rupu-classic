8546a99fba334fb00d5d6945f67811de9473c0ad
opbrutal <93695693+opbrutal@users.noreply.github.com>
2022-01-24 13:15:57 +0530

    Update youtube.py

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -5,14 +5,14 @@ from yt_dlp import YoutubeDL
 from config import BOT_NAME as bn, DURATION_LIMIT
 from helpers.errors import DurationLimitError
 
-ydl_opts = {
+ytdl_opts = {
     "format": "bestaudio/best",
     "verbose": True,
     "geo-bypass": True,
     "nocheckcertificate": True,
     "outtmpl": "downloads/%(id)s.%(ext)s",
 }
-ydl = YoutubeDL(ydl_opts)
+ytdl = YoutubeDL(ytdl_opts)
 
 
 def download(url: str) -> str:
@@ -24,5 +24,5 @@ def download(url: str) -> str:
             f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
         )
 
-    ydl.download([url])
+    ytdl.download([url])
     return path.join("downloads", f"{info['id']}.{info['ext']}")
